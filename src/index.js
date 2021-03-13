7ea62abbb64a2b5d5e635cea19394ec3106b990f
yoshikazu-ishitsuka <69833398+yoshikazu-ishitsuka@users.noreply.github.com>
2021-03-13 16:35:50 +0900

    letの練習

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 /*
   const,let等の変数宣言
 */
-var val1 = "var変数";
-console.log(val1);
+// var val1 = "var変数";
+// console.log(val1);
 
-// var変数は上書き可能
-val1 = "var変数を上書き";
-console.log(val1);
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
 
-// var変数は再宣言可能
-var val1 = "var変数を再宣言";
-console.log(val1);
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+let val2 = "let変数";
+console.log(val2);
+
+// letは上書きが可能
+val2 = "let変数を上書き";
+console.log(val2);
+
+// letは再宣言不可能
+let val2 = "let変数を再宣言";
