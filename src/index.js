7c9dbd72ee125e16f354d22b8f838838b135d410
yoshikazu-ishitsuka <69833398+yoshikazu-ishitsuka@users.noreply.github.com>
2021-03-13 16:45:32 +0900

    constの練習

--- src/index.js
+++ src/index.js
@@ -12,12 +12,36 @@
 // var val1 = "var変数を再宣言";
 // console.log(val1);
 
-let val2 = "let変数";
-console.log(val2);
+// let val2 = "let変数";
+// console.log(val2);
 
-// letは上書きが可能
-val2 = "let変数を上書き";
-console.log(val2);
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
 
-// letは再宣言不可能
-let val2 = "let変数を再宣言";
+// // letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+const val3 = "const変数";
+console.log(val3);
+
+// const変数は上書き不可能
+// val3 = "const変数を上書き";
+
+const val3 = "const変数を再宣言";
+
+オブジェクトにするとその中身は変更可能;
+constで定義したオブジェクトはプロパティの変更が可能;
+const val4 = {
+  name: "yos",
+  age: 28
+};
+// console.log(val4);
+val4.name = "jak";
+val4.address = "Ibaraki";
+// console.log(val4);
+// constで定義した配列はプロパティの変更が可能
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
