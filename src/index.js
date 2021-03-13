13ee7199bb01dc89d8a7f0694b68e943a2efd531
yoshikazu-ishitsuka <69833398+yoshikazu-ishitsuka@users.noreply.github.com>
2021-03-13 16:32:33 +0900

    var変数の練習

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/*
+  const,let等の変数宣言
+*/
+var val1 = "var変数";
+console.log(val1);
+
+// var変数は上書き可能
+val1 = "var変数を上書き";
+console.log(val1);
+
+// var変数は再宣言可能
+var val1 = "var変数を再宣言";
+console.log(val1);
