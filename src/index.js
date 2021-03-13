fb727b3a432273abd0bf2b1e484aec4830f28ac4
yoshikazu-ishitsuka <69833398+yoshikazu-ishitsuka@users.noreply.github.com>
2021-03-13 17:10:57 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -50,14 +50,34 @@
  * テンプレート文字列
  文字列の中にjsの変数を埋め込める
  */
-const name = "よすぃ";
-const age = 28;
-// 「私の名前はよすぃです。年齢は28歳です。」
+// const name = "よすぃ";
+// const age = 28;
+// // 「私の名前はよすぃです。年齢は28歳です。」
 
-// 従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
-// console.log(message1);
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// // console.log(message1);
 
-//　テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// //　テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+/**
+ *　アロー関数
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+// アロー関数
+const func2 = (str) => str;
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => num1 + num2;
+
+console.log(func3(10, 20));
