65b9296102f2109d441df7f6b5fcd1d7ca9f194d
yoshikazu-ishitsuka <69833398+yoshikazu-ishitsuka@users.noreply.github.com>
2021-03-13 20:31:32 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -69,15 +69,41 @@
 // function func1(str) {
 //   return str;
 // }
-const func1 = function (str) {
-  return str;
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// // アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) => num1 + num2;
+
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+const myProfile = {
+  name: "jake",
+  age: 28
 };
-console.log(func1("func1です"));
 
-// アロー関数
-const func2 = (str) => str;
-console.log(func2("func2です"));
+const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
 
-const func3 = (num1, num2) => num1 + num2;
+const { name, age } = myProfile;
 
-console.log(func3(10, 20));
+const message2 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
+
+const myProfile = ["じゃけぇ", 28];
+
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+console.log(message3);
+
+// 順番で設定される
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}歳です`;
+console.log(message4);
