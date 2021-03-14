99e20aba8743c1098be95fba271bf795fbfc9061
yoshikazu-ishitsuka <69833398+yoshikazu-ishitsuka@users.noreply.github.com>
2021-03-14 09:52:01 +0900

    デフォルト値、引数

--- src/index.js
+++ src/index.js
@@ -85,25 +85,31 @@
 /**
  * 分割代入
  */
-const myProfile = {
-  name: "jake",
-  age: 28
-};
+// const myProfile = {
+//   name: "jake",
+//   age: 28
+// };
 
-const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
-// console.log(message1);
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// // console.log(message1);
 
-const { name, age } = myProfile;
+// const { name, age } = myProfile;
 
-const message2 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
 
-const myProfile = ["じゃけぇ", 28];
+// const myProfile = ["じゃけぇ", 28];
 
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-console.log(message3);
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
 
-// 順番で設定される
-const [name, age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}歳です`;
-console.log(message4);
+// // 順番で設定される
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+sayHello("jake");
