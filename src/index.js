3e3cbd3aa9f9db12e496637bd1ca3118cef9e745
yoshikazu-ishitsuka <69833398+yoshikazu-ishitsuka@users.noreply.github.com>
2021-03-14 10:59:06 +0900

    論理演算子の意味

--- src/index.js
+++ src/index.js
@@ -187,22 +187,45 @@
 // });
 // console.log(newNameArr);
 
+// /**
+//  * 三項演算子
+//  */
+// // ある条件 ？ 条件がtrueの時 ： 条件がfalseの時
+// const val1 = 1 < 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = 1300;
+// // 金額などを3桁区切りで表示してくれる関数(数値だけ)
+// console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています！" : "許容範囲内です";
+// };
+// console.log(checkSum(50, 40));
+
 /**
- * 三項演算子
+ * 論理演算子の本当の意味を知ろう && ||
  */
-// ある条件 ？ 条件がtrueの時 ： 条件がfalseの時
-const val1 = 1 < 0 ? "trueです" : "falseです";
-console.log(val1);
+const flag1 = true;
+const flag2 = true;
 
-const num = 1300;
-// 金額などを3桁区切りで表示してくれる関数(数値だけ)
-console.log(num.toLocaleString());
+if (flag1 || flag2) {
+  console.log("1か2はtrueになります");
+}
+if (flag1 && flag2) {
+  console.log("1も2もtrueになります");
+}
 
-const formattedNum =
-  typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
-console.log(formattedNum);
+// || は左側がfalseなら右側を返す
+const num = 100;
+const fee = num || "金額未設定です";
+console.log(fee);
 
-const checkSum = (num1, num2) => {
-  return num1 + num2 > 100 ? "100を超えています！" : "許容範囲内です";
-};
-console.log(checkSum(50, 40));
+// && は左側がtrueなら右側を返す
+const num2 = null;
+const fee2 = num2 && "なにか設定されました";
+console.log(fee2);
