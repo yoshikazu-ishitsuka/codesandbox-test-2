d1e3e590e6e3e975af7699dc3657e07b7c5380f7
yoshikazu-ishitsuka <69833398+yoshikazu-ishitsuka@users.noreply.github.com>
2021-03-15 10:15:06 +0900

    buttonタグ生成

--- src/todo.js
+++ src/todo.js
@@ -16,8 +16,20 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // button（完了）タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+
+  // button（削除）タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
+  console.log(deleteButton);
+
   // divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
