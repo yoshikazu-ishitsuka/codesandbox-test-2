fb9644f175ed83256d99d18c2dfc81478cbbc5ba
yoshikazu-ishitsuka <69833398+yoshikazu-ishitsuka@users.noreply.github.com>
2021-03-15 10:18:11 +0900

    タスクの追加機能（ボタン）

--- src/todo.js
+++ src/todo.js
@@ -19,12 +19,17 @@ const onClickAdd = () => {
   // button（完了）タグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
 
   // button（削除）タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
-
-  console.log(deleteButton);
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+  // console.log(deleteButton);
 
   // divタグの子要素に各要素を設定
   div.appendChild(li);
